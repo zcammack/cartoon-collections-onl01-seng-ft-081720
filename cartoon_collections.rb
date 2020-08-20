a7026f5e3da85e08d7b2853fa6fa7ae9d0e03772
Zackary Cammack <zack.cammack@gmail.com>
2020-08-20 20:21:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name|
+    puts name
+
 end
 
 def summon_captain_planet# code an argument here
