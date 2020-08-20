5f7d9d75175741b85ce71fc7d35cf2bf91d3e734
Zackary Cammack <zack.cammack@gmail.com>
2020-08-20 20:48:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer)
 end
 
 def long_planeteer_calls(call)
-  call.select{|i| i.length > 4}
+  call.any?{|i| i.length > 4}
 end
 
 def find_the_cheese# code an argument here
