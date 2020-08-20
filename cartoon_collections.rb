e3027c4e01016a427f3d974216c865d2a8abe1cd
Zackary Cammack <zack.cammack@gmail.com>
2020-08-20 21:05:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) {|name, idx|puts "/#{idx}.* #{name}/"}
+end
+def summon_captain_planet(planeteer)
+  planeteer.collect do |member|
+    member.capitalize + "!"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(call)
+  call.any?{|i| i.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(stringcheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  stringcheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
